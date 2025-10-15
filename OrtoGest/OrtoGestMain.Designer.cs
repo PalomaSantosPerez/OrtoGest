fef8e3358198b818549b7b4173bdf6ef6992cf9c
PalomaSantosPerez <santosperez.psp@gmail.com>
2025-10-15 12:20:17 +0200

    Añadido fondo Azure y Label de título OrtoGest configurado

--- OrtoGest/OrtoGestMain.Designer.cs
+++ OrtoGest/OrtoGestMain.Designer.cs
@@ -28,20 +28,41 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.lblTitulo = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.Font = new System.Drawing.Font("Segoe UI Semibold", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitulo.ForeColor = System.Drawing.Color.SteelBlue;
+            this.lblTitulo.Location = new System.Drawing.Point(294, 41);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(153, 45);
+            this.lblTitulo.TabIndex = 0;
+            this.lblTitulo.Text = "OrtoGest";
+            this.lblTitulo.Click += new System.EventHandler(this.label1_Click);
             // 
             // OrtoGestMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Azure;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblTitulo);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
             this.Name = "OrtoGestMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "OrtoGest - Gestión de Ortopedias";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblTitulo;
     }
 }
 
--- OrtoGest/OrtoGestMain.cs
+++ OrtoGest/OrtoGestMain.cs
@@ -16,5 +16,10 @@ namespace OrtoGest
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
